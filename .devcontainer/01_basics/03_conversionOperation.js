08a2319a3a6e371ca41cc7ecf9fe9505f87569b5
chauhanbalaji <145220137+chauhanbalaji@users.noreply.github.com>
2023-12-13 15:02:43 +0000

    conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+// let score ="24abc"
+
+ //console.log(typeof score)
+ //console.log(typeof (score));
+
+ //let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+
+ //"24" =>24
+ //"24abc"=> NaN
+ ///true =>; false=>0
+
+ //let isloggedIn = 1
+
+ //let booleanIsLoggedIn = Boolean(isloggedIn)
+ //console.log(booleanIsLoggedIn)
+
+
+ //1 =>true; 0 =>false
+ //"" => false
+ //"balaji" => true
+
+
+ let someNumber = 14
+
+ let stringNumber = String(someNumber)
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+
+
+
+
+//******************Operation **************** *//
+
+ 
+  let value = 3
+  let negValue = -value
+  //console.log(negValue);
+
+ // console.log(2+2);
+  //console.log(2-2);
+  //console.log(2*2);
+  //console.log(2**);
+  //console.log(2/3);
+  //console.log(2%3);
+
+  
+  let str1 ="hello"
+  let str2 = "bala"
+
+  let str3 = str1 + str2
+
+
+  //console.log("1" + 2 + 2  );
+
+//   console.log(true+);
+//   console.log(+true);
+//   console.log( +"");
+
+  let num1, num2, num3
+
+  num1 = num2 = num3 = 3 + 3
+
+  let gameCounter = 100
+  ++gameCounter;
+  console.log(gameCounter);
