a0624b6a3a04eb75e1c12bf2b8400e643c76cb89
chauhanbalaji <145220137+chauhanbalaji@users.noreply.github.com>
2023-12-13 16:09:56 +0000

    string

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,16 @@
+const name = "bala"
+const cashCount = 1200
+
+/* it is old techique write code  string*/
+// console.log( name + cashCount + " Value");
+
+console.log(`Hello my name is  ${name} and my cash count is ${cashCount}`);
+
+const gameName = new String(`balaji`)
+
+//  console.log(gameName[0]);
+//  console.log(gameName__proto__);
+//  console.log(gameName.length);
+//  console.log(gameName.toUpperCase());
+ 
+ console.log(gameName.charAt(2));
---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,59 @@
+// primitive 
+
+//  7 types : string, numbe, Boolean, undefined, Symbol, BigInt,
+  
+/* javascript is Dynamically type */
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+const bigNumber = 145555n
+
+// Reference (Non primitive)
+
+// Array, Object, Functions 
+
+
+const heros = ["ironman", "hulk", "thor"];
+ let myObj = {
+    name: "bala",
+    age: 18,
+}
+
+
+// *******************************************
+
+// stack (Primitive), Heap (Non-Primitive )
+
+
+let myName = "balaji"
+
+
+let anotherName = myName
+anotherName ="bala"
+
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "userAgoogle",
+    upi: "user"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "bal.com"
+ console.log(userOne.email);
+ console.log(userTwo.email);
