e23bff649ce92697c8206d5a889a25f3b029ee09
chauhanbalaji <145220137+chauhanbalaji@users.noreply.github.com>
2023-12-13 14:32:54 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as never versiom
+
+ // alert(3+3) // we are using nodeJS, not browser
+
+ console.log(3+
+    2);  
+ // code readability should be high
+
+ console.log("balaji");
+
+
+
+ let name = "balaji"
+ let age = "18"
+ let islogged = false
+
+
+ // number => 2 to power 52
+ // bigint
+ // string => ""
+ // boolean => true / false
+ // null => standalone value 
+ // undefined =>
+ // symbol => unique 
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined);  //undefined
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,23 @@
+const accountID = 1224
+let accountEmail = "chauhanbalaji"
+accountCity = "mumbai"
+
+
+// accountId = 2 //  not allowed
+
+
+accountEmail = "helloA.com"
+accountPassword = "4124"
+accountCity = "Thane"
+ 
+
+console.log(accountID);
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ */
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
